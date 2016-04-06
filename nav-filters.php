a6aaee7c1d1afc381fc79a333fa4598baa098614
Josh Nederveld <joshnederveld@gmail.com>
2016-04-06 16:29:37 -0400

    Only pre-set container if container arg is not present

--- nav-filters.php
+++ nav-filters.php
@@ -18,30 +18,35 @@
  * @link https://codex.wordpress.org/Function_Reference/wp_nav_menu
  *
  * @param array $args WP Nav Menu args.
+ *
  * @return array $args Args for the WP Nav Menu.
  */
 function cnp_wp_nav_menu_default_args( $args ) {
 
 	// This filter can be disabled if "suppress_filters" is set to TRUE in the $args array.
-	if ( isset( $args['suppress_filters'] ) && TRUE === $args['suppress_filters'] ) {
+	if ( isset( $args['suppress_filters'] ) && true === $args['suppress_filters'] ) {
 		return $args;
 	}
 
-	$args['fallback_cb'] = FALSE;
-	$args['container'] = 'nav';
-	$args['items_wrap'] = PHP_EOL.'%3$s';
+	$args['fallback_cb'] = false;
+	$args['items_wrap']  = PHP_EOL . '%3$s';
 
-	if ( !isset( $args['container_class'] ) ) {
+	if ( ! isset( $args['container'] ) ) {
+		$args['container'] = 'nav';
+	}
+
+	if ( ! isset( $args['container_class'] ) ) {
 		$args['container_class'] = sanitize_title( $args['menu'] );
 	}
 
-	if ( !isset( $args['strip_li'] ) ) {
-		$args['strip_li'] = TRUE;
+	if ( ! isset( $args['strip_li'] ) ) {
+		$args['strip_li'] = true;
 	}
 
 	return $args;
 
 }
+
 add_filter( 'wp_nav_menu_args', 'cnp_wp_nav_menu_default_args', 20, 1 );
 
 
@@ -60,23 +65,25 @@ add_filter( 'wp_nav_menu_args', 'cnp_wp_nav_menu_default_args', 20, 1 );
  *
  * @param string $nav_menu Nav menu markup.
  * @param object $args WP Nav Menu args.
+ *
  * @return array $nav_menu Adjusted nav menu markup.
  */
 function cnp_wp_nav_menu_markup( $nav_menu, $args ) {
 
-	if ( isset( $args->strip_li ) && TRUE === $args->strip_li ) {
+	if ( isset( $args->strip_li ) && true === $args->strip_li ) {
 
-		$find = array('><a','</a>','<li','</li');
-		$replace = array('','','<a','</a');
-		$nav_menu = str_replace($find, $replace, $nav_menu);
+		$find     = array( '><a', '</a>', '<li', '</li' );
+		$replace  = array( '', '', '<a', '</a' );
+		$nav_menu = str_replace( $find, $replace, $nav_menu );
 
-		$nav_menu = trim($nav_menu);
-		$nav_menu = str_replace("\r", "", $nav_menu);
-		$nav_menu = str_replace("\n", "", $nav_menu);
+		$nav_menu = trim( $nav_menu );
+		$nav_menu = str_replace( "\r", "", $nav_menu );
+		$nav_menu = str_replace( "\n", "", $nav_menu );
 
 	}
 
 	return $nav_menu;
 
 }
+
 add_filter( 'wp_nav_menu', 'cnp_wp_nav_menu_markup', 20, 2 );
